af2cdc17bfe69780c2e8eae9b9f84696b1920053
Igor Fuchs Pereira <igorfuchs111@gmail.com>
2026-03-17 22:49:38 -0300

    feat: Inserting Password Max Length

--- src/Application/Validators/RegisterRequestValidator.cs
+++ src/Application/Validators/RegisterRequestValidator.cs
@@ -11,16 +11,14 @@ public sealed class RegisterRequestValidator : AbstractValidator<RegisterRequest
 {
     public RegisterRequestValidator()
     {
-        RuleFor(x => x.Name)
-            .NotEmpty().WithMessage("Name is required.")
-            .Matches(@"^[a-zA-Z\s]+$").WithMessage("Name can only contain letters and spaces.");
-
         RuleFor(x => x.Email)
             .NotEmpty().WithMessage("Email is required.")
-            .EmailAddress().WithMessage("Invalid email format.");
+            .EmailAddress().WithMessage("Invalid email format.")
+            .MaximumLength(254).WithMessage("Email cannot exceed 254 characters.");
 
         RuleFor(x => x.Password)
             .NotEmpty().WithMessage("Password is required.")
+            .MaximumLength(128).WithMessage("Password cannot exceed 128 characters.")
             .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
             .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
             .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter.")
